b83e1f5982c07b2c3e24dc57ccaef0dc3d1aa3cb
Adibi <maadibi@ead.ubc.ca>
2018-12-03 17:45:59 -0800

    p meter

--- app.R
+++ app.R
@@ -2,12 +2,13 @@
 library(shinydashboard)
 
 ui <- dashboardPage(
-  dashboardHeader(title = "p-value worth"),
+  dashboardHeader(title = "p-Value Meter"),
   ## Sidebar content
   dashboardSidebar(
     sidebarMenu(
-      menuItem("p-value", tabName = "p-value", icon = icon("dashboard")),
-      menuItem("Widgets", tabName = "widgets", icon = icon("th"))
+      menuItem("Calculator", tabName = "p-value", icon = icon("dashboard")),
+      menuItem("About", tabName = "aboutTab", icon = icon("address-book", lib="font-awesome")),
+      menuItem("Terms", tabName = "termsTab", icon = icon("balance-scale", lib="font-awesome"))
     )
   ),
   dashboardBody(
@@ -16,6 +17,7 @@ ui <- dashboardPage(
       tabItem(tabName = "p-value",
               fluidRow(
                 valueBoxOutput("ppv"),
+                valueBoxOutput("npv"),
                 
                 numericInput("pvalue", label = h5("What is your p-value?"), 
                              value = 0.05, min = 0, max = 1, step = 0.01),
@@ -24,7 +26,7 @@ ui <- dashboardPage(
                 numericInput("beta", label = h5("Type II error rate (β) ?"), 
                              value = 0.2, min = 0, max = 1, step = 0.1),
                 
-                numericInput("R", label = h5("Replication rate"), 
+                numericInput("R", label = h5("Replication rate in similar studies"), 
                              value = 0.5, min = 0, max = 1, step = 0.01)
               )
               
@@ -43,17 +45,31 @@ server <- function(input, output) {
   output$ppv <- renderValueBox({
     # The downloadRate is the number of rows in pkgData since
     # either startTime or maxAgeSecs ago, whichever is later.
-    ppv <- input$R * (1 - input$beta) / (input$R - input$R*input$beta + input$pvalue)
+    R <- input$R / (1 - input$R)
+    ppv <- R * (1 - input$beta) / (R - R * input$beta + input$pvalue)
     ppv <- round (ppv, 4)
     
     valueBox(
       value = formatC(ppv, digits = 2, format = "f"),
       subtitle = "Probablity of true positive",
-      icon = icon("area-chart"),
+      icon = icon("user-plus"),
       color = "aqua"
     )
   })
 
+  output$npv <- renderValueBox({
+    # The downloadRate is the number of rows in pkgData since
+    # either startTime or maxAgeSecs ago, whichever is later.
+    R <- input$R / (1 - input$R)
+    npv <- (1 - input$pvalue) / (R * input$beta + 1 - input$pvalue)
+
+    valueBox(
+      value = formatC(npv, digits = 2, format = "f"),
+      subtitle = "Probablity of true negative",
+      icon = icon("user-minus"),
+      color = "red"
+    )
+  })
 }
 
 shinyApp(ui, server)
