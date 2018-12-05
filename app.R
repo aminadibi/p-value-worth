421fd866b2f5147ecce56a52622d247dc26098ad
Adibi <maadibi@ead.ubc.ca>
2018-12-05 13:55:29 -0800

    version 1

--- app.R
+++ app.R
@@ -1,8 +1,11 @@
 ## app.R ##
 library(shinydashboard)
+library(scales)
+library(shinythemes)
+
 
 ui <- dashboardPage(
-  dashboardHeader(title = "p-Value Meter"),
+  dashboardHeader(title = "p-meter"),
   ## Sidebar content
   dashboardSidebar(
     sidebarMenu(
@@ -15,26 +18,42 @@ ui <- dashboardPage(
     tabItems(
       # First tab content
       tabItem(tabName = "p-value",
+              includeMarkdown("intro.Rmd"),
+              valueBoxOutput("ppv", width = 12),
+              tags$b("Enter inputs:"),
+              #valueBoxOutput("npv"),
               fluidRow(
-                valueBoxOutput("ppv"),
-                valueBoxOutput("npv"),
-                
-                numericInput("pvalue", label = h5("What is your p-value?"), 
-                             value = 0.05, min = 0, max = 1, step = 0.01),
-          
-
-                numericInput("beta", label = h5("Type II error rate (β) ?"), 
-                             value = 0.2, min = 0, max = 1, step = 0.1),
-                
-                numericInput("R", label = h5("Replication rate in similar studies"), 
-                             value = 0.5, min = 0, max = 1, step = 0.01)
-              )
+           
+                column(4, offset = 0.8, 
+                  "p-value", tags$span(style="color:red", "*"), ":",       
+                  sliderInput("pvalue", width = '100%', label = "", 
+                             value = 0.05, min = 0, max = 0.05, step = 0.001)
+                ),
               
+                column(4, 
+                     "Statistical Power (1-β)", tags$span(style="color:red", "*"), ":",
+                     sliderInput("power", width = '100%', label = "", 
+                                  value = 0.8, min = 0, max = 1, step = 0.1)
+
+                ),
+              
+                column(4, 
+                       "Prior probability (%)", tags$span(style="color:red", "*"), ":",
+                       sliderInput("R", width = '100%', label = "", 
+                                  value = 69, min = 0, max = 100, step = 1)
+                )
+              
+              ),
+              #includeMarkdown("intro.Rmd"),
+              includeMarkdown("methods.Rmd")
       ),
       
-      # Second tab content
-      tabItem(tabName = "widgets",
-              h2("Widgets tab content")
+      tabItem(tabName = "aboutTab",
+              includeMarkdown("about.Rmd")
+      ),
+      
+      tabItem(tabName = "termsTab",
+              includeMarkdown("disclaimer.Rmd")
       )
     )
   )
@@ -45,13 +64,14 @@ server <- function(input, output) {
   output$ppv <- renderValueBox({
     # The downloadRate is the number of rows in pkgData since
     # either startTime or maxAgeSecs ago, whichever is later.
-    R <- input$R / (1 - input$R)
-    ppv <- R * (1 - input$beta) / (R - R * input$beta + input$pvalue)
-    ppv <- round (ppv, 4)
+    R <- input$R / (100 - input$R)
+    beta <- 1 - input$power
+    ppv <- scales::percent(R * (1 - beta) / (R - R * beta + input$pvalue))
+    #ppv <- round (ppv, 4)
     
     valueBox(
-      value = formatC(ppv, digits = 2, format = "f"),
-      subtitle = "Probablity of true positive",
+      value = formatC(ppv, digits = 1, format = "f"),
+      subtitle = "Positive Predictive Value (probablity of true positive)",
       icon = icon("user-plus"),
       color = "aqua"
     )
@@ -60,8 +80,9 @@ server <- function(input, output) {
   output$npv <- renderValueBox({
     # The downloadRate is the number of rows in pkgData since
     # either startTime or maxAgeSecs ago, whichever is later.
-    R <- input$R / (1 - input$R)
-    npv <- (1 - input$pvalue) / (R * input$beta + 1 - input$pvalue)
+    R <- input$R / (100 - input$R)
+    beta <- 1 - input$power
+    npv <- (1 - input$pvalue) / (R * beta + 1 - input$pvalue)
 
     valueBox(
       value = formatC(npv, digits = 2, format = "f"),
